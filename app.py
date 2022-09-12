d93fc60c1f4e7aa1cf72617534614bc8cafebce6
Donatas Noreika <donoras@gmail.com>
2022-09-12 09:04:31 +0300

    Pridėtas balanso paskaičiavimas

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def home():
 def biudzetas():
     return render_template('biudzetas.html', zurnalas=zurnalas)
 
+@app.route("/balansas")
+def balansas():
+    return render_template('balansas.html', balansas=sum(zurnalas))
+
 @app.route("/prideti", methods=['GET', 'POST'])
 def prideti():
     if request.method == "POST":
