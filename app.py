3a4cf4335377ef8f0079a666c1b8da739cd9067d
Donatas Noreika <donoras@gmail.com>
2022-09-12 09:01:38 +0300

    pridėtas biudžeto atvaizdavimas, įrašo pridėjimas

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from biudzetas import zurnalas
+from flask import Flask, render_template, request, redirect
+from biudzetas import zurnalas, irasyti_zurnala
 
 app = Flask(__name__)
 
@@ -10,6 +10,15 @@ def home():
 @app.route("/biudzetas")
 def biudzetas():
     return render_template('biudzetas.html', zurnalas=zurnalas)
+
+@app.route("/prideti", methods=['GET', 'POST'])
+def prideti():
+    if request.method == "POST":
+        suma = float(request.form['suma'])
+        zurnalas.append(suma)
+        irasyti_zurnala()
+        return redirect("biudzetas")
+    return render_template('prideti.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- biudzetas.py
+++ biudzetas.py
@@ -1,3 +1,20 @@
 
-zurnalas = []
+import pickle
 
+def gauti_zurnala():
+    try:
+        with open("zurnalas.pkl", 'rb') as file:
+            zurnalas = pickle.load(file)
+            return zurnalas
+    except:
+        with open("zurnalas.pkl", 'wb') as file:
+            zurnalas = []
+            pickle.dump(zurnalas, file)
+            return zurnalas
+
+def irasyti_zurnala():
+    with open("zurnalas.pkl", 'wb') as file:
+        pickle.dump(zurnalas, file)
+
+
+zurnalas = gauti_zurnala()
